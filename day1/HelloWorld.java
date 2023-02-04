d1cfaec9872d9992a12d13b785a7cef21911a886
Sbanerjee471 <124444388+Sbanerjee471@users.noreply.github.com>
2023-02-04 19:04:30 +0530

    Add files via upload
    Day 1

---
+++ day1/HelloWorld.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		int a =5;
+		boolean b = true;
+		String c = "shubhajyoti";
+		
+System.out.println(a);
+System.out.println(b);
+System.out.println(c);	
+	}
+	
+
+}
