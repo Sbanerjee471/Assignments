1d24482159afb031b235bf13da20db7e75a01493
Sbanerjee471 <124444388+Sbanerjee471@users.noreply.github.com>
2023-02-04 19:27:24 +0530

    Add files via upload
    Twowheeler

---
+++ day1/TwoWheller.java
@@ -0,0 +1,20 @@
+package week1.day1.assignments;
+
+public class TwoWheller {
+public static void main(String[] args) {
+	
+	int numberOfWheels =2;
+	short numberOfMirrors=2;
+	long chesisNumber =1234567890l;
+	boolean isPunctured =false;
+	String bikeName ="R15V3";
+	double runningKm =120.30;
+	System.out.println(numberOfWheels);
+	System.out.println(numberOfMirrors);
+	System.out.println(chesisNumber);
+	System.out.println(isPunctured);
+	System.out.println(bikeName);
+	System.out.println(runningKm);
+	
+		}
+}
