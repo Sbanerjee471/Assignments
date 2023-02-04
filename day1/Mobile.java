3925f26e54a2b0a33d4063ed30b69d4cdd780e1c
Sbanerjee471 <124444388+Sbanerjee471@users.noreply.github.com>
2023-02-04 19:41:41 +0530

    Mobile

---
+++ day1/Mobile.java
@@ -0,0 +1,27 @@
+package week1.day1.assignments;
+
+public class Mobile {
+	public static void main(String[] args) {
+		String mobileBrandName = "XIAOMI";
+		char mobileLogo = '!';
+		short noOfMobilePiece =31569;
+		int modelNumber =65986;
+		long mobileImeiNumber =659863254l;
+		float mobilePrice = 25500.3675f;
+boolean isDamaged = false;
+System.out.println(mobileBrandName);
+System.out.println(mobileLogo);
+System.out.println(noOfMobilePiece);
+System.out.println(modelNumber);
+System.out.println(mobileImeiNumber);
+System.out.println(mobilePrice);
+System.out.println(isDamaged);
+
+
+		
+		
+		
+		
+	}
+
+}
